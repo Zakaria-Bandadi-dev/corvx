1aa3b452c2a066324e8657d1bd0bb78bdd49cb7b
Zakaria-Bandadi-dev <z.bandadi@edu.umi.ac.ma>
2026-08-10 20:28:42 +0100

    Remove job sites from job_sites.py
    Removed several job site entries from the private and abroad categories.

--- config/job_sites.py
+++ config/job_sites.py
@@ -2,18 +2,14 @@ JOB_SITES = {
     "private_ma": {
         "label": "الخدمة فالمغرب (القطاع الخاص)",
         "sites": [
-            ("LinkedIn", "https://www.linkedin.com/jobs/jobs-in-morocco/"),
             ("ReKrute", "https://www.rekrute.com/"),
-            ("Emploitic", "https://emploitic.com/"),
             ("Dreamjob", "https://www.dreamjob.ma/"),
-            ("Careerlink", "https://careerlink.ma/"),
         ],
     },
     "public_ma": {
         "label": "الوظائف العمومية (المغرب)",
         "sites": [
             ("Emploi Public", "https://www.emploi-public.ma/"),
-            ("ANAPEC", "https://www.anapec.org/"),
         ],
     },
     "gulf": {
@@ -26,8 +22,6 @@ JOB_SITES = {
     "abroad": {
         "label": "الخدمة فالخارج (أوروبا / كندا)",
         "sites": [
-            ("ANAPEC Infitah", "https://skills.ma/infitah/"),
-            ("EURES", "https://eures.europa.eu/"),
             ("France Travail", "https://candidat.francetravail.fr/"),
             ("Guichet-Emplois Canada", "https://www.guichetemplois.gc.ca/"),
         ],
